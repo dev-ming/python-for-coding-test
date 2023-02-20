27ba33543d9741c6643b4f29c576c3a4dd395d6b
dev-ming <ruaaaa@ajou.ac.kr>
2023-02-21 01:09:36 +0900

    :sparkles: add

--- Greedy/큰 수의 법칙.py
+++ Greedy/큰 수의 법칙.py
@@ -1,17 +1,33 @@
+import time
 n, m, k = map(int, input().split())
 data = list(map(int, input().split()))
+
+starttime = time.time()
 
 data.sort()
 first = data[-1]
 second = data[-2]
 
-result = 0
+# 내 코드
+# result = 0
 
-while (m):
-    for i in range(k):
-        result += first
-        m -= 1
-    result += second
-    m -= 1
+# while (m):
+#     for i in range(k):
+#         result += first
+#         m -= 1
+#     result += second
+#     m -= 1
+
+# 답 코드 -> 큰 수가 더해지는 횟수를 계산
+count = int(m/(k+1))*k
+count += m % (k+1)
+
+result = 0
+result += (count) * first
+result += (m-count)*second
 
 print("result", result)
+
+endtime = time.time()
+a = endtime-starttime
+print(a)
